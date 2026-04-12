a6ea7d0a254f65d417cf884d944273c790337419
lsoriagrf <lsoriagrefa@gmail.com>
2026-04-12 14:59:13 -0500

    first commit

---
+++ src/app/contact-card/contact-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-card',
+  imports: [],
+  templateUrl: './contact-card.html',
+  styleUrl: './contact-card.css',
+})
+export class ContactCard {}
